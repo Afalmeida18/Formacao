05b972b5578aa09bdfd41a007ec6aeb69af5afb6
Afonso Almeida <afalmeida@deloitte.pt>
2023-02-15 12:17:00 +0000

    Closed <p> in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        </p>
            <h1>Hello BrightStaAAAAAARTS!</h1>
       </header>
     </div>
