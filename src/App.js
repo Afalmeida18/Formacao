c439698aac794ca23341c8058df1e758ff9a4bb7
Afonso Almeida <afalmeida@deloitte.pt>
2023-02-15 12:13:56 +0000

    Changed text in app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        </p>
-     <h1>Hello BrightStarts!</h1>
+           <h1>Hello BrightStaAAAAAARTS!</h1>
       </header>
     </div>
   );
